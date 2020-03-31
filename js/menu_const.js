1e11d39821184b9819198f120815ad8c3e6de9b5
xuansr <842704182@qq.com>
2020-03-31 09:49:57 +0800

    zkx增加权限js

---
+++ js/menu_const.js
@@ -0,0 +1,21 @@
+//页面菜单映射【自行维护】
+var menuMapping = {
+	"jggl001": "guahao.html",
+	"jggl002": "huajia.html",
+	"jggl003": "shoufei.html",
+	"khda001": "chufang.html",
+	"khda002": "yonghu.html",
+	"qcwx001": "juese.html",
+	"qcwx002": "juese.html",
+	"qt001": "guahao.html",
+	"qt002": "huajia.html",
+	"rs001": "shoufei.html",
+	"rs002": "chufang.html",
+	"rs003": "yonghu.html",
+	"rs004": "juese.html",
+	"xtgl001": "guahao.html",
+	"zsj001": "huajia.html",
+	"zsj002": "shoufei.html",
+	"zsj003": "chufang.html",
+	"zsj004": "yonghu.html"
+}
